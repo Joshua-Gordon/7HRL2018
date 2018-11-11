b338dbd63389eb78d68107b671ed30b2e492997f
Geometer1729 <kuhnsbt@clarkson.edu>
2018-11-11 18:56:29 -0500

    stairs probably

--- Main.hs
+++ Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import System.Random
-import System.Process
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
@@ -18,13 +17,11 @@ import Util
 main :: IO ()
 --main = sMain
 main = do
-  system "cd intro; python2 release.py"
   lv' <- genLevel 33 59
   let lv = boundLevel lv'
   alien <- spaceman (5,5)
-  let testWorld = Overworld lv startingPlayer [alien]
-  let testbattle = Battle testWorld startingPlayer [alien] (True,True) (State None AttackM) True
-  playIO FullScreen red 5 testbattle renderWorld handleInput idstep
+  let testWorld = Overworld lv startingPlayer [alien{nameM="stairs"},allien]
+  playIO FullScreen red 5 testWorld renderWorld handleInput idstep
 
 idstep _ w@(Overworld l p lm) = do
   let newMonsters = map (\m -> monsterThink m w) lm
@@ -38,7 +35,7 @@ idstep _ w@(Overworld l p lm) = do
     return testWorld
   else do
   return $ Overworld l p newMonsters
-idstep f b = stepBattle f b
+  
 
 stepBattle :: Float -> World -> IO World
 stepBattle _ here@(Battle w p ms sel (State pa ma) turn) | null ms = return w
@@ -46,14 +43,12 @@ stepBattle _ here@(Battle w p ms sel (State pa ma) turn) | null ms = return w
                                                     | otherwise = if turn then case pa of
                                                       Run -> return w
                                                       Attack -> do
-                                                        print "Attack!"
                                                         g <- getStdGen
-                                                        let (idx,g') = choice g [0..length ms-1]-- :: (Int,StdGen)
+                                                        let (idx,g') = choice g [0..length ms]-- :: (Int,StdGen)
                                                         let m = attackMob p (ms !! idx)
-                                                        print "Attacked"
                                                         return $ case m of
                                                           Just m' -> Battle w p (replace ms idx m') sel (State None ma) False
                                                           Nothing -> Battle w p (remove ms idx) sel (State None ma) False
-                                                      --Item _ -> return here
-                                                      --Ability _ -> return here
+                                                      Item _ -> return here
+                                                      Ability _ -> return here
                                                       else return here
