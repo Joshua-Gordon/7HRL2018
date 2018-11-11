58722b0264d91d24123da42a77a2d08fc60e5a2f
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-11-11 12:18:53 -0500

    Level is no longer made of bagels

--- Level.hs
+++ Level.hs
@@ -7,12 +7,22 @@ type Level = [[Tile]]
 
 data Tile = Floor | Wall | Object --Have Object include function for interact
 
-randBool :: IO Boolean
+randBool :: IO Bool
 randBool = do
           g <- getStdGen
-          let x = randomR (0,1) g
-          return x == 1
+          let (x,_) = randomR (0,1) g :: (Int,StdGen)
+          return $ x == 1
+
+chunk :: Int -> [a] -> [[a]]
+chunk n xs = take n xs : chunk n (drop n xs)
 
 genLevel :: Int -> Int -> IO Level
 genLevel x y = do
-              g <- getStdGen
+              boolsflat <- replicateM (x*y) randBool
+              let bools = chunk x boolsflat
+              let levelFirst = [[if (bools !! m !! n) then Floor else Wall | m <- [0..x]] | n <- [0..y]]
+              return $ generate 100 levelFirst
+
+--runs the cellular aut
+generate :: Int -> Level -> Level
+generate = undefined
