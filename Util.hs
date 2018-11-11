612f6432f90bc1c7692939088a6341e783fa748f
Graham Northup <grissess@nexusg.org>
2018-11-11 16:11:08 -0500

    Adding offset utilities

--- Util.hs
+++ Util.hs
@@ -2,9 +2,21 @@ module Util where
 
 type Pos = (Int, Int)
 
+zeroPos :: Pos
+zeroPos = (0, 0)
+
 chunk :: Int -> [a] -> [[a]]
 chunk _ [] = [[]]
 chunk n xs = take n xs : chunk n (drop n xs)
 
 
 data Heading = Up | Down | Left | Right
+
+applyHeading :: Pos -> Heading -> Int -> Pos
+applyHeading (x,y) Up d = (x, y+d)
+applyHeading (x,y) Down d = (x, y-d)
+applyHeading (x,y) Left d = (x-d, y)
+applyHeading (x,y) Right d = (x+d, y)
+
+headingOff :: Heading -> Int -> Pos
+headingOff = applyHeading zeroPos
